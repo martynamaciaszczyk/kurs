e2a5d196d2a8b5134eef509cf12fa196c6f7b128
martynamaciaszczyk <maciaszczyk.martyna@gmail.com>
2018-03-03 15:56:12 +0100

    php

---
+++ kurs/php/zmienne1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $liczba=100;
+        $drugaLiczba=10.5;
+        $tekst="Egzamin E.14";
+
+        /*echo "<br>";
+        echo $drugaLiczba;
+        echo "<br>";
+        echo $tekst;
+        echo "<br";*/
+echo $liczba."<br>".$drugaLiczba."<br>".$tekst."<br>";
+echo $liczba,"<br>",$drugaLiczba,"<br>",$tekst,"<br>";
+echo "$liczba<br>$drugaLiczba<br>$tekst<br>";
+
+    ?>
+
+</body>
+</html>
